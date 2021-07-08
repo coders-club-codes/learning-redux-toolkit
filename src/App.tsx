c68666145363c8dc4234110c18cac8dcb4c15981
Luke Morales <lukemorales@live.com>
2021-07-08 02:21:37 -0300

    feat: configure redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Provider } from 'react-redux';
+
 import Main from './pages/Main';
+import { store } from './store';
 
 function App() {
-  return <Main />;
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
 }
 
 export default App;
